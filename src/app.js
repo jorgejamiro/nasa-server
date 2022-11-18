9c67dc38e4bae332b3ae9839a303202a5bd74795
unknown <jorgejaykay@hotmail.com>
2022-11-18 12:38:17 +0100

    configuring server files

--- src/app.js
+++ src/app.js
@@ -8,11 +8,15 @@ import api from './routes/api.js';
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://nasa-server-ztm.herokuapp.com/',
 }));
 app.use(morgan('combined'));
 app.use(express.json());
 
 app.use('/v1/', api);
+
+app.get('/', (req, res) => {
+    res.send('APP IS RUNNING.');
+});
 
 export default app;
