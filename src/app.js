06604315ba9e65f17aaf3cda83b86feb11834246
unknown <jorgejaykay@hotmail.com>
2022-11-18 13:13:23 +0100

    solving CORS issue v1

--- src/app.js
+++ src/app.js
@@ -7,9 +7,12 @@ import api from './routes/api.js';
 
 const app = express();
 
-app.use(cors({
-    origin: 'https://nasa-server-ztm.herokuapp.com/',
-}));
+//app.use(cors({
+//    origin: 'https://nasa-server-ztm.herokuapp.com/',
+//}));
+
+app.use(cors());
+
 app.use(morgan('combined'));
 app.use(express.json());
 
